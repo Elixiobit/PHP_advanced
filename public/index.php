4ae18411faa793ef3b29315201bedaafe8f13098
Ilya Sushkov <pyh3@yandex.ru>
2020-06-11 00:40:00 +0300

    lesson2_start

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<pre>
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . "/../services/Autoloader.php";
+
+spl_autoload_register([new Autoloader(), 'loadClass']);
+
+$product = new Product();
+$product->setCategoryId()
+    ->setDescription();
+
+function foo(ModelInterface $object){
+    var_dump($object->getById());
+}
+
+var_dump($product);
