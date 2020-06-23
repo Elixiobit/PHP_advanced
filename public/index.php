bfe1282c21bc06ee82d4e5963a021bcfef6dd6c2
Ilya Sushkov <pyh3@yandex.ru>
2020-06-23 14:14:48 +0300

    lesson4_start

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
+//Lesson4
 require $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php";
 require ROOT_DIR . "services/Autoloader.php";
 
 spl_autoload_register([new app\services\Autoloader(), 'loadClass']);
 
 //(new \app\services\Db())->getConnection(); //использовался для
-
 
 $product = (new \app\models\Product())->getById(1);
 
